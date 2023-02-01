faf63312f2210b19ac5cc672275f246f98db0a0f
dani saputra <mochamad.saputra@ist.id>
2023-02-01 14:03:34 +0700

    class EnvironmentTest

--- tests/Feature/EnvironmentTest.php
+++ tests/Feature/EnvironmentTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class EnvironmetnTest extends TestCase
+class EnvironmentTest extends TestCase
 {
     public function testGetEnv()
     {
