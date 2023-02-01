172622f0f31d3ce447b512e6794d148e6971a2e4
dani saputra <mochamad.saputra@ist.id>
2023-02-01 15:21:32 +0700

    class config

---
+++ config/contoh-dani.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    "name" => [
+        "first" => env('NAME_FIRST', "dani"),
+        "last" => env("NAME_LAST", "ganteng")
+    ],
+    "email" => "danniisaputra@gmail.com"
+];
