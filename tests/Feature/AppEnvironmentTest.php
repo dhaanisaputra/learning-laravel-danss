d89a0a49d0cf9adfe2fa2082f1f348a40cc3cd8c
dani saputra <mochamad.saputra@ist.id>
2023-02-01 15:18:43 +0700

    class AppEnvironmentTest

---
+++ tests/Feature/AppEnvironmentTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\App;
+use Tests\TestCase;
+
+class AppEnvironmentTest extends TestCase
+{
+    public function testAppEnv() 
+    {
+        // var_dump(App::environment()); //cek using app_env
+
+        if(App::environment('testing'))
+        {
+            self::assertTrue(true);
+        }
+    }
+}
