e5c3fee870a8f16fd4b0fd54598684b1fe46f078
dani saputra <mochamad.saputra@ist.id>
2023-02-01 14:55:49 +0700

    class EnvironmentTest

--- tests/Feature/EnvironmentTest.php
+++ tests/Feature/EnvironmentTest.php
@@ -4,14 +4,24 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Env;
 use Tests\TestCase;
+
+use function PHPUnit\Framework\assertEquals;
 
 class EnvironmentTest extends TestCase
 {
     public function testGetEnv()
     {
-        $learnlrv = env('LearnLaravel');
+        $learn = env('LearnLaravel');
+        self::assertEquals('Belajar Laravel', $learn);
+        
+    }
 
-        self::assertEquals('Belajar Laravel', $learnlrv);
+    public function testDefaultEnv() 
+    {
+        // $author = env('AUTHOR', 'dani');
+        $author = Env::get('AUTHOR', 'dani');
+        self::assertEquals('dani', $author);
     }
 }
