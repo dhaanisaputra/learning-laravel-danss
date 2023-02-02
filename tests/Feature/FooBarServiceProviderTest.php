3522863ad4a6443cc6e232f468965a1181db0174
dani saputra <mochamad.saputra@ist.id>
2023-02-02 18:02:04 +0700

    service Provider

---
+++ tests/Feature/FooBarServiceProviderTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Data\Bar;
+use App\Data\Foo;
+use App\Services\HelloService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertSame;
+
+class FooBarServiceProviderTest extends TestCase
+{
+    public function testServiceProvider()
+    {
+        $foo1 = $this->app->make(Foo::class);
+        $foo2 = $this->app->make(Foo::class);
+
+        self::assertSame($foo1, $foo2);
+
+        $bar1 = $this->app->make(Bar::class);
+        $bar2 = $this->app->make(Bar::class);
+
+        self::assertSame($bar1, $bar2);
+
+        self::assertSame($foo1, $bar1->foo);
+        self::assertSame($foo2, $bar2->foo);
+    }
+
+    public function testPropertySingleton()
+    {
+        $helloservice1 = $this->app->make(HelloService::class);
+        $helloservice2 = $this->app->make(HelloService::class);
+
+        self::assertSame($helloservice1, $helloservice2);
+
+        self::assertEquals('Halo dani', $helloservice1->hello('dani'));
+    }
+
+}
