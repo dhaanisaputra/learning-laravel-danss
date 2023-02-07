571235c1c134b3fc8116b7629031a8c24efb97b9
dani saputra <mochamad.saputra@ist.id>
2023-02-07 13:05:59 +0700

    class Route Parameter

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,34 @@ Route::get('hello-again', function () {
 Route::get('hello-world', function () {
     return view('hello.world', ['name' => 'Dani']);
 });
+
+//Route with param
+Route::get('/products/{id}', function ($productId) {
+    return "Product $productId";
+});
+
+Route::get('/products/{products}/items/{item}', function ($productId, $itemId) {
+    return "Product $productId, Items $itemId";
+});
+
+//Route with regex
+Route::get('/categories/{id}', function (string $categoryId) {
+   return "Categories : ".$categoryId;
+})->where('id', '[0-9]+');
+
+//Route param optional
+Route::get('/users/{id?}', function ($userId = '404') {
+    return "User $userId";
+});
+
+
+//Route param conflict, prioritas adalah yg paling atas
+Route::get('/conflict/{name}',function ($name) {
+   return "Conflict $name";
+});
+
+Route::get('/conflict/dans', function () {
+    return "Conflict dani ganteng";
+});
+
+
--- tests/Feature/RoutingTest.php
+++ tests/Feature/RoutingTest.php
@@ -27,5 +27,38 @@ class RoutingTest extends TestCase
             ->assertSeeText('404 by dansss');
     }
 
+    public function testRouteParameter()
+    {
+        $this->get('products/1')
+            ->assertSeeText('Product 1');
 
+        $this->get('products/2')
+            ->assertSeeText('Product 2');
+
+        $this->get('products/1/items/XXX')
+            ->assertSeeText("Product 1, Items XXX");
+
+        $this->get('products/2/items/YYY')
+            ->assertSeeText("Product 2, Items YYY");
+    }
+
+    public function testRouteParameterRegex()
+    {
+        $this->get('/categories/123456')->assertSeeText("Categories : 123456");
+        $this->get('/categories/salah')->assertSeeText("404 by dansss");
+    }
+
+    public function testRouteParameterOptional()
+    {
+        $this->get('/users/alpha')->assertSeeText("User alpha");
+        $this->get('/users/')->assertSeeText("User 404");
+    }
+
+    public function testRouteParameterConflict() {
+        $this->get('/conflict/coba')
+            ->assertSeeText("Conflict coba");
+
+        $this->get('conflict/dans')
+            ->assertSeeText("Conflict dani ganteng");
+    }
 }
